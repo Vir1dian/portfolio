44216986f4c5c69365603cd6b340d069eeafb3f6
Vir1dian <132039432+Vir1dian@users.noreply.github.com>
2025-06-24 10:56:05 -0700

    setting up background styling and removed some of the initial boilerplate files

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,67 @@
 import Bubbles from './components/background/Bubbles.vue';
 import CosmeticGrid from './components/background/CosmeticGrid.vue';
 import Waves from './components/background/Waves.vue';
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/starstruck.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="page-wrapper">
+    <div class="content">
+      <h1>Welcome to My Portfolio!</h1>
+      <p>This is some example content to demonstrate the background matching its height.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sint esse, hic expedita beatae iusto nemo voluptates neque nostrum excepturi eius maxime harum reprehenderit quasi, ipsa officiis totam illo. Laudantium voluptates esse culpa autem sunt rem est totam! Nobis inventore iste fugiat vel odit! Facere aut rerum eius enim repellendus natus, consequatur ipsa necessitatibus unde velit! Accusantium et nesciunt excepturi quisquam placeat obcaecati soluta fugiat consequuntur voluptates eligendi tenetur quas culpa dolores officiis, accusamus dolorum autem cupiditate maxime dignissimos harum molestias, dolorem numquam iure fugit? Sed facilis quam doloremque! Veritatis laboriosam suscipit iusto explicabo necessitatibus saepe temporibus et veniam quis! Laborum, ex non atque illo error assumenda, perspiciatis qui hic odit neque quibusdam similique id nihil necessitatibus quis sapiente vitae fugiat sunt a recusandae, quidem est. Aspernatur repellendus, laudantium odit iste totam incidunt aliquid fuga vel inventore omnis culpa aperiam voluptate molestiae autem unde deleniti doloremque esse nobis vitae dolorem cupiditate. Ratione architecto dolore vel neque consectetur commodi deleniti numquam, quod, rerum perferendis, saepe deserunt accusantium. Nihil at, numquam ipsam aut eum itaque dignissimos eos quibusdam aperiam! Voluptate ad dolorum aperiam, eos ab pariatur, aspernatur voluptates nisi, ut quas sapiente quo rem sed numquam similique enim vero suscipit ipsa perspiciatis? Atque, velit possimus! Eius assumenda ipsa, tempora enim atque repudiandae nobis, sunt beatae maxime cupiditate nulla sapiente doloremque amet incidunt in qui placeat? Enim sit nihil eveniet maxime nostrum, dicta iure! Explicabo porro esse, sit in aliquam deleniti tempore possimus facere libero qui fugit sequi. Neque nostrum, harum perferendis autem sit itaque. Tempora in veritatis doloribus necessitatibus, accusantium consectetur accusamus iure adipisci velit aperiam iusto porro. Corporis, ullam nobis itaque voluptatem eius repellat temporibus at odit asperiores explicabo, consequuntur esse voluptates quibusdam facere totam. Enim, molestiae optio quos ea ipsum quaerat labore provident a quisquam nesciunt voluptatum nam ullam incidunt, consequuntur ab tenetur quasi distinctio voluptatibus suscipit sed reiciendis placeat atque! Laborum explicabo accusantium nisi. Adipisci eius totam minus ad dolore, ullam accusantium doloribus reiciendis eaque ea ipsum. Dolorum maxime, quidem repudiandae facilis aspernatur eius rem temporibus, nesciunt eum animi dicta enim incidunt necessitatibus reprehenderit nemo. Iure modi illo facere ducimus facilis, excepturi, exercitationem, molestias quos rerum labore alias sapiente enim maxime. Sequi sapiente eius error obcaecati quae placeat unde eos alias commodi nobis praesentium distinctio soluta adipisci assumenda dolores quis ad magnam, voluptatibus voluptate voluptatum. Impedit laudantium unde, dolores expedita magni deleniti obcaecati a illo nulla, similique ab ex, aliquam quis repudiandae neque aspernatur officiis ipsam iure ratione rerum. Minima quaerat earum voluptate, natus officiis est, enim aperiam eligendi alias velit hic? Id sint quae corporis voluptates labore, eum iure quasi suscipit praesentium quibusdam necessitatibus quis minus? Atque numquam, perspiciatis dolorum quidem, dolores excepturi repellendus nam adipisci corporis nostrum ullam veniam expedita animi! Nemo, earum. Iusto, quia quam ullam porro debitis praesentium molestias numquam nihil rem facilis ut esse possimus minima provident nulla eum cumque mollitia, architecto explicabo velit dolorem inventore autem saepe? Omnis, quidem, corporis eos perferendis totam porro id laborum doloribus fugiat iste nostrum alias ducimus dignissimos. Ipsam, accusantium cupiditate cumque odit dicta consectetur iusto totam! Consectetur atque rem, commodi quos aliquid, veniam asperiores voluptas, porro eos laboriosam aperiam modi qui! Minima explicabo odio molestias eaque tempore esse recusandae! Tempora quo illum, in soluta animi obcaecati sunt, qui reprehenderit maxime exercitationem quisquam dolores? Maiores nostrum explicabo dolore sed ad dolorum blanditiis sunt quisquam commodi necessitatibus numquam veniam, veritatis doloribus animi, quam sequi praesentium. Illum voluptatem iure dolores molestiae excepturi provident nihil. Reiciendis magnam vel aut natus asperiores nemo fugit provident explicabo ratione repudiandae dolores eligendi iusto, voluptates dolore ex dolorem, sequi qui omnis consequatur voluptas dignissimos exercitationem! Perferendis tenetur error quod, quasi saepe beatae enim cumque, nemo nam praesentium maxime? Illo ad dolore sapiente, fugiat totam quasi harum. Fuga aliquid quis voluptatibus itaque, architecto at perspiciatis debitis provident repellendus porro sint dolorem, nostrum animi eaque eius? Aspernatur delectus repellat incidunt expedita nihil odit maxime minus provident voluptas neque, ratione quaerat earum magnam tenetur dicta laboriosam illum porro, voluptates pariatur? Accusamus esse qui enim dolore voluptate vel illum suscipit maxime amet quos laboriosam blanditiis temporibus, nesciunt neque nobis. Dignissimos incidunt veritatis officiis debitis molestias sequi eum temporibus laboriosam reiciendis, fugiat inventore voluptatibus doloribus! Error laudantium itaque dolor culpa impedit cupiditate deleniti debitis repellat. Aperiam, corporis consequatur numquam veritatis, aut quasi vitae asperiores placeat expedita omnis vero voluptas suscipit obcaecati, accusamus atque non nostrum hic dolorem! Alias nisi, dolorem esse, quae qui quidem sed iste voluptates cum provident totam, nihil est atque ullam eum ipsa! Beatae corrupti minima fugiat ducimus ad dolores numquam? Natus officia repellat animi ex amet facere voluptates a eum. Ab tenetur quisquam et consequuntur incidunt sunt at voluptate doloremque, qui aperiam placeat reprehenderit magni eaque quas officia aliquid architecto totam fugit voluptas perferendis accusantium! Itaque laborum quaerat nostrum. Aspernatur deserunt excepturi dolor iure dignissimos, fuga fugiat maxime, provident voluptatum, ducimus laudantium vel. Quis, amet! Saepe eum delectus quisquam consequatur pariatur amet, eligendi repellat exercitationem molestias dolore corrupti optio culpa ea adipisci error, quidem quasi vel nemo odit! Totam similique vel est neque impedit eligendi, accusantium sapiente beatae necessitatibus quia fugiat ea ab? Labore qui ut culpa mollitia impedit ipsam amet laboriosam itaque autem, corrupti atque facilis et? Assumenda nemo delectus velit, tenetur expedita perferendis maiores culpa vitae blanditiis atque dignissimos magni eveniet adipisci, veritatis quia necessitatibus obcaecati? Magnam sint voluptas debitis assumenda accusantium perspiciatis pariatur omnis mollitia id qui, sed non distinctio ad quo voluptates nam quaerat aliquam minima cumque nesciunt. Fugit facere quae repellat, modi at obcaecati fugiat dolor voluptate dolore iusto maiores repudiandae aspernatur, incidunt ipsum dicta quis aut possimus pariatur, expedita aperiam earum non! Mollitia, laborum suscipit? Nostrum, incidunt nihil? Sed eum magni sapiente excepturi voluptatem, doloremque earum nulla consequatur nesciunt, rerum suscipit accusamus quis cumque repellendus odio molestias! Quod perspiciatis totam quis, labore voluptatibus illum? Tempore tempora saepe molestias sed impedit consequatur maxime, qui ad quasi asperiores, illum sint labore numquam. Quas suscipit nihil inventore fugiat, soluta quia ipsam velit dolores? Odio blanditiis repellendus voluptatem ipsum aspernatur natus voluptates aliquid aliquam!</p>
+      <p>And another one for good measure!</p>
+      </div>
+    <div class="background">
+      <Bubbles />
+      <CosmeticGrid />
+      <Waves />
+    </div>
   </div>
-  <Bubbles />
-  <CosmeticGrid />
-  <Waves />
-  <HelloWorld />
   <!-- probably have a temporary width of 1000vh or more to test the flow of scrolling through it -->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.page-wrapper {
+  display: grid;
+  /* Define a single grid area named 'stack' */
+  grid-template-areas: "stack";
+  min-height: 100vh; /* Ensure wrapper takes at least full viewport height */
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.content {
+  grid-area: stack;
+  position: relative; 
+  z-index: 1;
+  padding: 20px; 
+  box-sizing: border-box;
+
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.background {
+  grid-area: stack;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: #DBF1F4;
+}
+
+/* Background components, e.g. Bubbles, CosmeticGrid, Waves */
+.background > * {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
 }
 </style>
