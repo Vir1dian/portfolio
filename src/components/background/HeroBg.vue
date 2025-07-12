269223e1ff25c38ef146a43d727610f14decafa9
Vir1dian <132039432+Vir1dian@users.noreply.github.com>
2025-07-11 21:41:22 -0700

    small styling changes to hero

--- src/components/background/HeroBg.vue
+++ src/components/background/HeroBg.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import BubbleOrbits from './BubbleOrbits.vue';
+import DottedGrid from './DottedGrid.vue';
 
 interface Props {
   color: string;
@@ -21,9 +22,10 @@ const herobg_style = computed(() => {
 <template>
 
   <div class="hero-background" :style="herobg_style">
+    <DottedGrid :color="'6AFFED'" class="herobg-grid" />
     <BubbleOrbits 
       :count="10"
-      :width="1080"
+      :width="1440"
       :height="650"
       :ang_velocity="{ min: 0.05, max: 0.25 }"
       :bubble_radius="{ min: 75, max: 180 }"
@@ -49,5 +51,10 @@ const herobg_style = computed(() => {
   left: 0;
 
   border-bottom: solid 2px #0E6875;
+}
+.herobg-grid {
+  position: absolute;
+  -webkit-mask-image: radial-gradient(ellipse, rgba(0, 0, 0, 0) 55%, rgba(0, 0, 0, 1) 75%);
+  mask-image: radial-gradient(ellipse, rgba(0, 0, 0, 0) 55%, rgba(0, 0, 0, 1) 75%);
 }
 </style>
